bdeeaffdb2c65b1bdb36d7f0e47c30c4b075f246
jihannanda <71716088+jihannanda@users.noreply.github.com>
2020-12-13 08:34:09 +0700

    Update and rename main.js to javascript.js

---
+++ javascript.js
@@ -0,0 +1,7 @@
+var n = prompt("Selamat datang, tuliskan nama:");
+var c = confirm("Hello " + n + " اهلا و سهلا في معهدنا");
+if (c == true) {
+    alert('Good Luck');
+} else {
+    alert('No Problem');
+}
